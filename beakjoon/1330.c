0893e192323c39379ffea99b7f6abe0c1f515bf6
dododo1224 <kdymmm1224@naver.com>
2022-07-15 00:04:36 +0900

    1330

---
+++ beakjoon/1330.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main(void){
+    int A;
+    int B;
+    scanf("%d %d",&A,&B);
+    if (A > B){
+        printf(">");
+    }
+    if (A < B){
+        printf("<");
+    }
+    if (A == B){
+        printf("==");
+    }
+    return 0;
+}
