9943d37aaff8fd0387cf854361e71cd76812c2f9
dododo1224 <kdymmm1224@naver.com>
2022-07-18 15:21:31 +0900

    2439

---
+++ beakjoon/2439.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main (void){
+    int i,j,N;
+    scanf("%d", &N);
+    for(j=1; j<=N; j++){
+        for(i=N; i>j; i--){
+            printf(" ");
+        }
+        for(i=0; i<j; i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
