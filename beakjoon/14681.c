272c710f0423367dc2facba633cad15eb3f49c8f
dododo1224 <kdymmm1224@naver.com>
2022-07-22 19:58:57 +0900

    14681

---
+++ beakjoon/14681.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main (void){
+    int x;
+    int y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    if(x>0){
+        if(y>0){printf("1");}
+        else{printf("4");}
+    }
+    else if(x<0){
+        if(y>0){printf("2");}
+        else{printf("3");}
+    }
+    return 0;
+}
